a4d57afa034ebac6a75fffa471082339c65158ac
palomasarda <palomaasarda@gmail.com>
2024-10-10 12:27:17 -0300

    voy a descansar un ratito, hice algunas cosas

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 //import AlumnoViewScreen from '@/src/Alumnos/AlumnoViewScreen';
-import PadresScreen from '@/src/Padres/PadresViewScreen';
+import PadresScreen from '@/src/Login/Login';
 const IndexScreen: React.FC = () => {
   return (
     <View style={{ flex: 1 }}>
