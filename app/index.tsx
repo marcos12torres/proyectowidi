3ae99e3a596f8b5805306ef549316bc3a57e3463
palomasarda <palomaasarda@gmail.com>
2024-10-13 17:58:10 -0300

    login con google

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
-//import AlumnoViewScreen from '@/src/Alumnos/AlumnoViewScreen';
-import PadresScreen from '@/src/Login/Login';
+import { View, Text } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+/*import App from './Navigation';
+import MyTabs from './Navigation';
+import Nav from './Navigation';*/
+import inicio from '../src/inicio';
+import App from '../src/Navigation';
+
+/*
+import Login from './Login';
+const Stack = createNativeStackNavigator();
+*/
+
 const IndexScreen: React.FC = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <PadresScreen></PadresScreen>
-    </View>
+ <App/>
   );
 };
 
 export default IndexScreen;
+
+
+
+
