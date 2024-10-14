5234aedf93047895874f27d5685ccda097f5c9ae
Nicoljara <aldanajara06@gmail.com>
2024-10-14 00:14:17 -0300

    Eliminando registro

---
+++ app/NavigationContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Tus pantallas aquí */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
