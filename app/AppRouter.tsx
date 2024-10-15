6a375587400a02fdae1c51a7d7188d16ffe910a5
demuzzaporfa <carinamar06@gmail.com>
2024-10-15 14:54:27 -0300

    Oveja 19: cambiamos redirect por navigate

--- app/AppRouter.tsx
+++ app/AppRouter.tsx
@@ -1,5 +1,8 @@
+/*Reemplazo de Switch por Routes: Se ha reemplazado todas las instancias de Switch por Routes, que es el componente 
+correspondiente en las versiones más recientes de react-router-dom. */
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext'; // Importa tu contexto de autenticación
 import HomeScreen from './inicio';   // Pantalla de inicio
 import LoginScreen from './Login'; // Pantalla de login
@@ -14,10 +17,10 @@ const AppRouter: React.FC = () => {
         <Route path="/home" element={user ? <HomeScreen /> : <LoginScreen />} />
 
         {/* Ruta para login: si está autenticado, lo redirige a HomeScreen */}
-        <Route path="/login" element={user ? <Redirect to="/home" /> : <LoginScreen />} />
+        <Route path="/login" element={user ? <Navigate to="/home" /> : <LoginScreen />} />
 
         {/* Redirige cualquier otra ruta a /login */}
-        <Route path="*" element={<Redirect to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
