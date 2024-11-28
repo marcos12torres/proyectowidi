ff960f1687bb0ee6aff8a593aeb8c832c8afc10b
demuzzaporfa <carinamar06@gmail.com>
2024-11-28 17:56:39 -0300

    guardo x si las dudas jijiji

--- app/auth/firebase.tsx
+++ app/auth/firebase.tsx
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-export { auth, provider, db };
+export { auth, provider, db, app };
--- screens/AcercaDeNosotros.tsx
+++ screens/AcercaDeNosotros.tsx
@@ -8,9 +8,11 @@ import React from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, ImageBackground } from 'react-native';
 import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import { auth } from '../app/auth/firebase';
+import { getFirestore } from 'firebase/firestore';
+import { app } from '../app/auth/firebase';
 
 const { width } = Dimensions.get('window');
+const db = getFirestore(app);
 
 const AcercaDeNosotros = () => { //llamando a AcercaDeNosotros.tsx
   const equipo = [
@@ -251,6 +253,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   valorCard: {
+    width: width * 0.4,
     alignItems: 'center',
     padding: 15,
   },
